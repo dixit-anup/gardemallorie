1e420282fe53457a72370916ff94d32846faab80
david-deck <deck.david@gmail.com>
2014-03-05 22:37:48 +0100

    v2

--- src/main/java/com/appspot/gardemallorie/web/ApplicationConversionServiceFactoryBean.java
+++ src/main/java/com/appspot/gardemallorie/web/ApplicationConversionServiceFactoryBean.java
@@ -1,8 +1,11 @@
 package com.appspot.gardemallorie.web;
 
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.support.FormattingConversionServiceFactoryBean;
 import org.springframework.roo.addon.web.mvc.controller.converter.RooConversionService;
+
+import com.appspot.gardemallorie.domain.BabySitter;
 
 /**
  * A central place to register application converters and formatters. 
@@ -15,4 +18,13 @@ public class ApplicationConversionServiceFactoryBean extends FormattingConversio
 		super.installFormatters(registry);
 		// Register application converters and formatters
 	}
+
+    public Converter<BabySitter, String> getBabySitterToStringConverter() {
+        return new org.springframework.core.convert.converter.Converter<com.appspot.gardemallorie.domain.BabySitter, java.lang.String>() {
+            public String convert(BabySitter babySitter) {
+                return babySitter.getFirstName();
+            }
+        };
+    }
+    
 }
