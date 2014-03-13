1d13e9bb7badef57babb66a10d68cfdb21a6149b
david-deck <deck.david@gmail.com>
2014-03-13 21:11:58 +0100

    BabySittings default sort field = day

--- src/main/java/com/appspot/gardemallorie/reference/BabySitterType.java
+++
@@ -1,6 +0,0 @@
-package com.appspot.gardemallorie.reference;
-
-public enum BabySitterType {
-
-    JOSEPHINE, EMILIE, BETTY, GISELE, DAVID, _, X
-}
--- src/main/java/com/appspot/gardemallorie/web/BabySittingController.java
+++ src/main/java/com/appspot/gardemallorie/web/BabySittingController.java
@@ -1,38 +1,73 @@
 package com.appspot.gardemallorie.web;
+
 import java.util.Date;
 
-import com.appspot.gardemallorie.domain.BabySitting;
-
+import org.springframework.roo.addon.web.mvc.controller.finder.RooWebFinder;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.roo.addon.web.mvc.controller.finder.RooWebFinder;
 
-@RequestMapping("/babysittings")
+import com.appspot.gardemallorie.domain.BabySitting;
+
 @Controller
+@RequestMapping("/babysittings")
 @RooWebScaffold(path = "babysittings", formBackingObject = BabySitting.class)
 @RooWebFinder
 public class BabySittingController {
+	
+	public static final String BABYSITTINGS_ATTR = "babysittings";
+	public static final String BABYSITTINGS_LIST_VIEW = BABYSITTINGS_ATTR + "/list";
+	public static final String DEFAULT_SORT_FIELD = "day";
+	public static final String DEFAULT_PAGE_SIZE = "100";
+	public static final String MAX_PAGE_ATTR = "maxPages";
+	public static final String PAGE_PARAM = "page";
+	public static final String PAGE_SIZE_PARAM = "size";
+	public static final String SORT_FIELD_PARAM = "sortField";
+	public static final String SORT_ORDER_PARAM = "sortOrder";
 
-    void addDateTimeFormatPatterns(Model uiModel) {
-        uiModel.addAttribute("babySitting_day_date_format", "EEE dd/MM/yyyy");
-        uiModel.addAttribute("babySitting_plannedbeginning_date_format", "HH:mm");
-        uiModel.addAttribute("babySitting_plannedend_date_format", "HH:mm");
-        uiModel.addAttribute("babySitting_declaredend_date_format", "HH:mm");
+    @RequestMapping(params = "find=Next")
+    public String findNextBabySittings(
+		@RequestParam(value = PAGE_PARAM, required = false) Integer page,
+		@RequestParam(value = PAGE_SIZE_PARAM, required = false, defaultValue = DEFAULT_PAGE_SIZE) Integer size,
+		@RequestParam(value = SORT_FIELD_PARAM, required = false) String sortFieldName,
+		@RequestParam(value = SORT_ORDER_PARAM, required = false) String sortOrder,
+		Model uiModel)
+    {
+    	Date day = new Date();
+        if (page != null || size != null) {
+            int sizeNo = size == null ? 10 : size.intValue();
+            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+            uiModel.addAttribute(BABYSITTINGS_ATTR, BabySitting.findBabySittingsByDayGreaterThanEquals(day, sortFieldName, sortOrder).setFirstResult(firstResult).setMaxResults(sizeNo).getResultList());
+            float nrOfPages = (float) BabySitting.countFindBabySittingsByDayGreaterThanEquals(day) / sizeNo;
+            uiModel.addAttribute(MAX_PAGE_ATTR, (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+        } else {
+            uiModel.addAttribute(BABYSITTINGS_ATTR, BabySitting.findBabySittingsByDayGreaterThanEquals(day, sortFieldName, sortOrder).getResultList());
+        }
+        addDateTimeFormatPatterns(uiModel);
+        return BABYSITTINGS_LIST_VIEW;
     }
     
-    @RequestMapping(value = "/next", produces = "text/html")
-    public String findNextBabySittings(@RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
-        int sizeNo = 200;
-        final int firstResult = 0;
-        Date day = new Date();
-        uiModel.addAttribute("babysittings", BabySitting.findBabySittingsByDayGreaterThanEquals(day, sortFieldName, sortOrder).setFirstResult(firstResult).setMaxResults(sizeNo).getResultList());
-        float nrOfPages = (float) BabySitting.countFindBabySittingsByDayGreaterThanEquals(day) / sizeNo;
-        uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+    @RequestMapping(produces = "text/html")
+    public String list(
+		@RequestParam(value = PAGE_PARAM, required = false) Integer page,
+		@RequestParam(value = PAGE_SIZE_PARAM, required = false, defaultValue = DEFAULT_PAGE_SIZE) Integer size,
+		@RequestParam(value = SORT_FIELD_PARAM, required = false, defaultValue = DEFAULT_SORT_FIELD) String sortFieldName,
+		@RequestParam(value = SORT_ORDER_PARAM, required = false) String sortOrder,
+		Model uiModel)
+    {
+        if (page != null || size != null) {
+            int sizeNo = size == null ? 10 : size.intValue();
+            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+            uiModel.addAttribute(BABYSITTINGS_ATTR, BabySitting.findBabySittingEntries(firstResult, sizeNo, sortFieldName, sortOrder));
+            float nrOfPages = (float) BabySitting.countBabySittings() / sizeNo;
+            uiModel.addAttribute(MAX_PAGE_ATTR, (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+        } else {
+            uiModel.addAttribute(BABYSITTINGS_ATTR, BabySitting.findAllBabySittings(sortFieldName, sortOrder));
+        }
         addDateTimeFormatPatterns(uiModel);
-        return "babysittings/list";
+        return BABYSITTINGS_LIST_VIEW;
     }
     
 }
