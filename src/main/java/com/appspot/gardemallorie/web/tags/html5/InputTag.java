b7ce8a130a7959c26c83cbf5d1ef40fbf56fd744
david-deck <deck.david@gmail.com>
2014-04-06 12:29:20 +0200

    Refactoring Submit button Reset button

--- src/main/java/com/appspot/gardemallorie/web/tags/html5/InputTag.java
+++ src/main/java/com/appspot/gardemallorie/web/tags/html5/InputTag.java
@@ -1,8 +1,27 @@
 package com.appspot.gardemallorie.web.tags.html5;
 
+import javax.servlet.jsp.JspException;
+
+import org.springframework.web.servlet.tags.form.TagWriter;
+
 public class InputTag extends org.springframework.web.servlet.tags.form.InputTag {
 
 	private static final long serialVersionUID = 3750325455941455442L;
+
+	/**
+	 * The name of the '{@code required}' attribute.
+	 */
+	public static final String REQUIRED_ATTRIBUTE = "required";
+
+	private String required;
+
+	public String getRequired() {
+		return required;
+	}
+
+	public void setRequired(String required) {
+		this.required = required;
+	}
 
 	@Override
 	protected String getType() {
@@ -12,6 +31,20 @@ public class InputTag extends org.springframework.web.servlet.tags.form.InputTag
 		}
 		else {
 			return type.toString();
+		}
+	}
+
+	protected boolean isRequired() throws JspException {
+		return evaluateBoolean(REQUIRED_ATTRIBUTE, getRequired());
+	}
+
+	@Override
+	protected void writeOptionalAttributes(TagWriter tagWriter) throws JspException {
+
+		super.writeOptionalAttributes(tagWriter);
+
+		if (isRequired()) {
+			writeOptionalAttribute(tagWriter, REQUIRED_ATTRIBUTE, REQUIRED_ATTRIBUTE);
 		}
 	}
 
